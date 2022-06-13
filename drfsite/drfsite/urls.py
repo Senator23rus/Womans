eeba36c803f4d5cb99bd6efdfdabdd3e21de8ae2
senator23 <senator295@gmail.com>
2022-06-13 21:45:37 +0300

    permissions

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -5,13 +5,20 @@ from django.urls import path, include
 from women.views import *
 from  rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'women', WomenViewSet, basename='women')
-print(router.urls)
+# router = routers.DefaultRouter()
+# router.register(r'women', WomenViewSet, basename='women')
+# print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+
+
+
+
+    # path('api/v1/', include(router.urls))
     # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
     # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 
--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -8,6 +8,8 @@ from .models import Women
 from rest_framework import serializers
 
 class WomenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Women
         fields = ('__all__')
